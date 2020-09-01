2d6e6aec5244c2b064792627570e3a872d023a31
Ethan Malenchek <emalenchek@gmail.com>
2020-09-01 13:57:36 -0400

    Added search bar filter functionality

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import { Container, Table } from 'reactstrap';
+import { Container, Table, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import { connect } from 'react-redux';
 import { getCountries } from '../actions/countryActions';
 import PropTypes from 'prop-types';
 
 class TableView extends Component {
 
+    state = {
+        searchTerm: ''
+    }
+
     componentDidMount() {
         this.props.getCountries();
     }
 
+    editSearchTerm = (e) => {
+        this.setState({searchTerm: e.target.value})
+    }
+
     render() {
-        const { countries } = this.props.country; 
+        const { countries } = this.props.country;
+        const filtered = countries.filter((data) => {
+            if(this.state.searchTerm === '') {
+                return data;
+            } else if(data.Country.toLowerCase().includes(this.state.searchTerm.toLowerCase())) {
+                return data;
+            }
+        }); 
 
         return(
             <div>
+                <Container>
+                    <InputGroup className="mb-5">
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>Search</InputGroupText>
+                        </InputGroupAddon>
+                        <Input onChange={this.editSearchTerm} />
+                    </InputGroup>
+                </Container>
                 <Container>
                     <h1>Totals By Country</h1>
                     <div className="table-responsive">
@@ -32,8 +55,8 @@ class TableView extends Component {
                             </thead>
                             <tbody>
                             {
-                            countries.map(({ Country, Slug, TotalConfirmed, TotalDeaths, TotalRecovered, NewConfirmed, NewDeaths, NewRecovered }) => (
-                                    <tr key={Slug}>
+                            filtered.map(({ Country, Slug, TotalConfirmed, TotalDeaths, TotalRecovered, NewConfirmed, NewDeaths, NewRecovered }) => (
+                                    <tr key={Slug} id={Slug}>
                                         <td className="font-weight-bold">{Country}</td>
                                         <td className="font-weight-bold">{TotalConfirmed}</td>
                                         <td className="font-weight-bold text-danger">{TotalDeaths}</td>
