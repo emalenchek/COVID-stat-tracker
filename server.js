61a924273f1e81d856257278f6451f2b8d477bed
Ethan Malenchek <emalenchek@gmail.com>
2020-09-04 09:01:42 -0400

    Set up CORS policy proxy

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/summary', (req, res) => {
+  request(
+    { url: 'https://api.covid19api.com/summary' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(process.env.PORT || 5000);
