458c8e64c5b7537ba1c0124ae9f2b7dcef2b13eb
Ethan Malenchek <emalenchek@gmail.com>
2020-11-30 15:34:12 -0500

    github link test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https:/github.com/emalenchek">GitHub</NavLink>
+                                    <NavLink to="https:/github.com/emalenchek">GitHub</NavLink>
                                 </NavItem>                            
                             </Nav>
                         </Collapse>
